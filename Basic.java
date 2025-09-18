4adda57366e236831240642b30fe551b14c80cfe
S.K.AKASH <ska9519976@gmail.com>
2025-09-18 19:54:30 +0530

    Add files via upload

---
+++ Basic.java
@@ -0,0 +1,12 @@
+public class Basic
+{
+	static int a = 10; //static variable
+	static int b = 20;//static variable
+	public static void main (String[] args) 
+	{
+		System.out.println("a="+ a);
+		System.out.println("b="+ b);
+		System.out.println("c="+(a+b));
+
+	}
+}
