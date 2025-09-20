bc9fe95974ee037f6f03b8ddb638976d0ca98d30
S.K.AKASH <ska9519976@gmail.com>
2025-09-20 22:17:01 +0530

    Add files via upload

---
+++ ControlStatement (1).java
@@ -0,0 +1,18 @@
+public class ControlStatement
+{
+	public static void main(String[] args)
+	{
+		int age = 25;
+		String name = "bob";
+
+		if(name == "baba") 
+		{
+			System.out.println("welcome bob");
+		} 
+		else
+		{
+			System.out.println("sorry, bob has access to his .try again!");
+		}
+	}
+}
+
---
+++ ControlStatement.java
@@ -0,0 +1,22 @@
+public class ControlStatement
+{
+	public static void main(String[] args)
+	{
+		int age = 25;
+		String name = "bob";
+
+		if(name == "bob") {
+			System.out.println("welcome bob");
+
+			if(age>=22) {
+				System.out.println("you'r are authenticate to access");
+			}
+			else {
+				System.out.println("need age to atleast 22!");
+			}
+		} else {
+			System.out.println("sorry, bob has access to his .try again!");
+		}
+	}
+}
+
