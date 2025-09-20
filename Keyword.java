914d939b54e16f75ef9a2149e14064d300957819
S.K.AKASH <ska9519976@gmail.com>
2025-09-20 19:23:01 +0530

    Add files via upload

---
+++ Keyword.java
@@ -0,0 +1,18 @@
+public class Keyword
+{
+    public static void main(String [] args)
+    {
+        int a =10;
+        float b =10.4f;
+        double c= 20000d;
+        char ch = 'A';
+        String st ="hello world";
+        boolean bool = true;
+        short s =100;
+        long lo = 100_00_00_00_000l;
+        byte by = -128;
+        System.out.println("numeric datatype--->"+ " integer = "+ a +" float = "+ b + " double = "+ c +" short = "+ s + " long = "+lo+" byte = "+ by);  
+        System.out.println("alphabets datatype --->"+ " char = "+ch+" string = "+st);
+        System.out.println("logical datatype --->"+" boolean --->"+bool);
+    }
+}
