d2e1424af9da8672bf95a6c0d3c0306c566cecb9
S.K.AKASH <ska9519976@gmail.com>
2025-09-21 16:38:08 +0530

    Add files via upload

---
+++ ternaryOperator.java
@@ -0,0 +1,14 @@
+
+public class ternaryOperator
+{
+	public static void main(String[] args) {
+
+		int i = 4;
+
+		//ternary operator
+
+		String PrimeNumber = i%2==0 ? "Even" : "odd";
+
+		System.out.println(PrimeNumber);
+	}
+}
