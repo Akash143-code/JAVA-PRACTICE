24e4dd8957d7a0aee2e07eab7eb01043d7d2bb19
S.K.AKASH <ska9519976@gmail.com>
2025-09-21 21:51:03 +0530

    Add files via upload

---
+++ Loops (2).java
@@ -0,0 +1,13 @@
+public class Loops
+{
+	public static void main(String[] args)
+	{
+		int i =100;
+
+		do
+		{
+			System.out.println("print at least once :");
+			i++;
+		}while(i<=4);
+	}
+}
---
+++ Loops (3).java
@@ -0,0 +1,13 @@
+public class Loops
+{
+	public static void main(String[] args)
+	{
+		
+
+		for(int i =10;i<=20;i++)
+		{
+			System.out.println("hi"+i);
+		
+		}
+	}
+}
